4403fafa0f5755af4d19029f810c470a14d3e936
Jan-Philipp Litza <jpl@plutex.de>
2022-07-21 14:17:59 +0200

    post-config: Support EdgeOS 1.x

--- post-config.sh
+++ post-config.sh
@@ -3,7 +3,7 @@
 set -eu
 
 SUDOERS="/etc/sudoers.d/02ubnt-hal-snmp"
-SUDOERS_LINE="Debian-snmp ALL = NOPASSWD: /usr/sbin/ubnt-hal"
+SUDOERS_LINE="Debian-snmp,snmp ALL = NOPASSWD: /usr/sbin/ubnt-hal"
 if ! [ -f "$SUDOERS" ] || ! grep -F "$SUDOERS_LINE" "$SUDOERS" > /dev/null; then
     echo "$SUDOERS_LINE" > "$SUDOERS"
     chmod 0440 "$SUDOERS"
@@ -11,7 +11,11 @@ fi
 
 SNMPD_CONF="/etc/snmp/snmpd.conf"
 SNMPD_LINE="pass .1.3.6.1.4.1.4413.1.1.43.1 /config/scripts/ubnt-hal-snmp.sh"
-if ! grep -Fx "$SNMPD_LINE" "$SNMPD_CONF" > /dev/null; then
+if ! grep -F "$SNMPD_LINE" "$SNMPD_CONF" > /dev/null; then
     echo "$SNMPD_LINE" >> "$SNMPD_CONF"
-    systemctl try-reload-or-restart snmpd
+    if command -v systemctl > /dev/null 2>&1; then
+        systemctl try-reload-or-restart snmpd
+    else
+        /etc/init.d/snmpd reload
+    fi
 fi
